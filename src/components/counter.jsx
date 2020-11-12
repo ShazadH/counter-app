60275e2703b10a5f8f3dad07564b981457699b62
Shazad Hoang <shazad.hoang@gmail.com>
2020-11-12 20:43:33 +0100

    first component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 export class Counter extends Component {
   render() {
-    return <h1>Hello World!!!</h1>;
+    return <h2>Hello World!!!</h2>;
   }
 }
 
